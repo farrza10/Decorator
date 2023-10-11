27872f9eeb93db31bb7aeb324d57d720357bcf85
farrza10 <70225671+farrza10@users.noreply.github.com>
2023-10-11 15:43:17 +0300

    Update Decorator.py

--- Decorator.py
+++ Decorator.py
@@ -1,3 +1,28 @@
+/---------args/kwargs in functions-------/
+
+class car():
+    
+    def __init__(self, *args):
+        self.speed = args[0]
+        self.color = args[1]
+        
+audi = car(200, "red")
+bmw = car(250, "black")
+mb = car(190, "white")
+# print(audi.color)
+# print(audi.speed)
+
+class car():
+    def __init__(self, **kwargs):
+        self.speed = kwargs["s"]
+        self.color = kwargs["c"]
+        
+audi = car(s=200, c='red')
+bmw = car(s=250, c='black')
+mb = car(s=190, c='white')
+print(audi.color)
+print(audi.speed)
+
 
 
 /----------multiple Args---------/
